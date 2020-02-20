edb2852aee065aa34c7da5f573e9ae6eb29e1c58
Patrick Saunders <45730159+PatASaunders@users.noreply.github.com>
2020-02-20 16:19:41 +0100

    Add files via upload

--- PIR1.R
+++ PIR1.R
@@ -2,10 +2,15 @@ library(AER)
 library("readxl")
 library(stargazer)
 library(ggplot2)
+library(olsrr)
+library(mlogit)
 
 data = read_excel("C:/Users/Patrice/Desktop/Classes/PIR/Dataset_V1.2.xlsx")
-data
-View(data)
+data_r <- 
+
+data3<-data[complete.cases(data),]
+data2
+View(data3)
 
 model = lm(log(City_conso)~Eng_Disp, data=data)
 model
@@ -51,7 +56,23 @@ model_MixConso=lm(log(Mix_Conso)~Eng_Disp+nCyl+nGears+Stop_Start+Fuel_Bin, data=
 model_MixConso
 coeftest(model_MixConso, vcov. = vcovHC, type="HC1")
 
+
 coeftest(model_HwyConso, vcov. = vcovHC, type="HC1")
 coeftest(model_CityConso, vcov. = vcovHC, type="HC1")
+
+ols_test_breusch_pagan(model_MixConso)
+
+model_CityConso2=lm(log(Hwy_Conso)~AMS+AM+SA+M+A+SCV+CVT+Fuel_Bin, data=data)
+model_CityConso2
+
+model_HwyConso2=lm(log(City_Conso)~AMS+AM+SA+M+A+SCV+CVT, data=data)
+model_HwyConso2
+
+lCity_Conso = log(data$City_Conso)
+
+qplot(Trans, City_Conso,data=data)
+
+model1<-lm(log(City_Conso)~AMS+AM+SA+A+SCV+CVT, data=data)
+model1
 
 
